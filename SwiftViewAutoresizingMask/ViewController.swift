fca8d7060de494ead7189f88dc8e3f3037d36884
文优 <wenyou.ly@taobao.com>
2016-09-27 14:42:10 +0800

    update

--- SwiftViewAutoresizingMask/ViewController.swift
+++ SwiftViewAutoresizingMask/ViewController.swift
@@ -16,11 +16,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.parentView = UIView()
-        self.parentView?.backgroundColor = UIColor.grayColor()
+        self.parentView?.backgroundColor = UIColor.gray
         self.view.addSubview(self.parentView!)
         
         self.subView = UIView()
-        self.subView?.backgroundColor = UIColor.purpleColor()
+        self.subView?.backgroundColor = UIColor.purple
         self.parentView?.addSubview(self.subView!)
         
         self.resetFrame()
@@ -28,57 +28,60 @@ class ViewController: UIViewController {
         var button: UIButton?
         
         button = self.createButton()
-        button?.setTitle("reset", forState: .Normal)
-        button?.frame = CGRectMake(10, self.view.frame.size.height - 40 * 3, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button0Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("reset", for: UIControlState())
+        button?.frame = CGRect(x: 10, y: self.view.frame.size.height - 40 * 3, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button0Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
         
         button = self.createButton()
-        button?.setTitle("none", forState: .Normal)
-        button?.frame = CGRectMake(10 * 2 + (self.view.frame.size.width - 40) / 3, self.view.frame.size.height - 40 * 3, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button1Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("none", for: UIControlState())
+        button?.frame = CGRect(x: 10 * 2 + (self.view.frame.size.width - 40) / 3, y: self.view.frame.size.height - 40 * 3, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button1Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
         
         button = self.createButton()
-        button?.setTitle("t | b | h", forState: .Normal)
-        button?.frame = CGRectMake(10 * 3 + (self.view.frame.size.width - 40) / 3 * 2, self.view.frame.size.height - 40 * 3, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button2Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("t | b | h", for: UIControlState())
+        var x = 10 * 3 + (self.view.frame.size.width - 40) / 3 * 2
+        button?.frame = CGRect(x: x, y: self.view.frame.size.height - 40 * 3, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button2Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
         
         button = self.createButton()
-        button?.setTitle("top", forState: .Normal)
-        button?.frame = CGRectMake(10, self.view.frame.size.height - 40 * 2, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button3Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("top", for: UIControlState())
+        button?.frame = CGRect(x: 10, y: self.view.frame.size.height - 40 * 2, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button3Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
         
         button = self.createButton()
-        button?.setTitle("bottom", forState: .Normal)
-        button?.frame = CGRectMake(10 * 2 + (self.view.frame.size.width - 40) / 3, self.view.frame.size.height - 40 * 2, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button4Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("bottom", for: UIControlState())
+        button?.frame = CGRect(x: 10 * 2 + (self.view.frame.size.width - 40) / 3, y: self.view.frame.size.height - 40 * 2, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button4Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
         
         button = self.createButton()
-        button?.setTitle("height", forState: .Normal)
-        button?.frame = CGRectMake(10 * 3 + (self.view.frame.size.width - 40) / 3 * 2, self.view.frame.size.height - 40 * 2, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button5Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("height", for: UIControlState())
+        x = 10 * 3 + (self.view.frame.size.width - 40) / 3 * 2
+        button?.frame = CGRect(x: x, y: self.view.frame.size.height - 40 * 2, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button5Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
         
         button = self.createButton()
-        button?.setTitle("t | b", forState: .Normal)
-        button?.frame = CGRectMake(10, self.view.frame.size.height - 40 * 1, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button6Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("t | b", for: UIControlState())
+        button?.frame = CGRect(x: 10, y: self.view.frame.size.height - 40 * 1, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button6Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
         
         button = self.createButton()
-        button?.setTitle("t | h", forState: .Normal)
-        button?.frame = CGRectMake(10 * 2 + (self.view.frame.size.width - 40) / 3, self.view.frame.size.height - 40 * 1, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button7Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("t | h", for: UIControlState())
+        button?.frame = CGRect(x: 10 * 2 + (self.view.frame.size.width - 40) / 3, y: self.view.frame.size.height - 40 * 1, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button7Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
         
         button = self.createButton()
-        button?.setTitle("b | h", forState: .Normal)
-        button?.frame = CGRectMake(10 * 3 + (self.view.frame.size.width - 40) / 3 * 2, self.view.frame.size.height - 40 * 1, (self.view.frame.size.width - 40) / 3, 30)
-        button?.addTarget(self, action: Selector("button8Click:"), forControlEvents: .TouchUpInside)
+        button?.setTitle("b | h", for: UIControlState())
+        x = 10 * 3 + (self.view.frame.size.width - 40) / 3 * 2
+        button?.frame = CGRect(x: x, y: self.view.frame.size.height - 40 * 1, width: (self.view.frame.size.width - 40) / 3, height: 30)
+        button?.addTarget(self, action: #selector(ViewController.button8Click(_:)), for: .touchUpInside)
         self.view.addSubview(button!)
     }
     
@@ -87,75 +90,75 @@ class ViewController: UIViewController {
     }
     
     func createButton() -> UIButton {
-        let button = UIButton.init(type: UIButtonType.Custom)
-        button.backgroundColor = UIColor.orangeColor()
-        button.autoresizingMask = .FlexibleTopMargin
+        let button = UIButton.init(type: UIButtonType.custom)
+        button.backgroundColor = UIColor.orange
+        button.autoresizingMask = .flexibleTopMargin
         button.layer.cornerRadius = 2;
         button.layer.masksToBounds = true
         button.layer.borderWidth = 1;
-        button.layer.borderColor = UIColor.darkGrayColor().CGColor;
+        button.layer.borderColor = UIColor.darkGray.cgColor;
         return button;
     }
     
     func resetFrame() {
-        self.parentView?.frame = CGRectMake(0, 100, self.view.frame.size.width, 200);
-        self.subView?.frame = CGRectMake(0, 50, self.view.frame.size.width, 100);
+        self.parentView?.frame = CGRect(x: 0, y: 100, width: self.view.frame.size.width, height: 200);
+        self.subView?.frame = CGRect(x: 0, y: 50, width: self.view.frame.size.width, height: 100);
     }
     
     func changeFrame() {
-        self.parentView?.frame = CGRectMake(0, 100, self.view.frame.size.width, 400);
+        self.parentView?.frame = CGRect(x: 0, y: 100, width: self.view.frame.size.width, height: 400);
     }
     
-    func button0Click(sender:UIButton) {
+    func button0Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = .None
+        self.subView?.autoresizingMask = UIViewAutoresizing()
     }
     
-    func button1Click(sender:UIButton) {
+    func button1Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = .None
+        self.subView?.autoresizingMask = UIViewAutoresizing()
         self.changeFrame()
     }
     
-    func button2Click(sender:UIButton) {
+    func button2Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = [.FlexibleTopMargin, .FlexibleBottomMargin, .FlexibleHeight]
+        self.subView?.autoresizingMask = [.flexibleTopMargin, .flexibleBottomMargin, .flexibleHeight]
         self.changeFrame()
     }
     
-    func button3Click(sender:UIButton) {
+    func button3Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = .FlexibleTopMargin
+        self.subView?.autoresizingMask = .flexibleTopMargin
         self.changeFrame()
     }
     
-    func button4Click(sender:UIButton) {
+    func button4Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = .FlexibleBottomMargin
+        self.subView?.autoresizingMask = .flexibleBottomMargin
         self.changeFrame()
     }
     
-    func button5Click(sender:UIButton) {
+    func button5Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = .FlexibleHeight
+        self.subView?.autoresizingMask = .flexibleHeight
         self.changeFrame()
     }
     
-    func button6Click(sender:UIButton) {
+    func button6Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = [.FlexibleTopMargin, .FlexibleBottomMargin]
+        self.subView?.autoresizingMask = [.flexibleTopMargin, .flexibleBottomMargin]
         self.changeFrame()
     }
     
-    func button7Click(sender:UIButton) {
+    func button7Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = [.FlexibleTopMargin, .FlexibleHeight]
+        self.subView?.autoresizingMask = [.flexibleTopMargin, .flexibleHeight]
         self.changeFrame()
     }
     
-    func button8Click(sender:UIButton) {
+    func button8Click(_ sender:UIButton) {
         self.resetFrame()
-        self.subView?.autoresizingMask = [.FlexibleBottomMargin, .FlexibleHeight]
+        self.subView?.autoresizingMask = [.flexibleBottomMargin, .flexibleHeight]
         self.changeFrame()
     }
 }
